09ebee24ccad9ec49ae574bebd26c3bf4d5b08f3
Luci Brănescu <luci.branescu@canonical.com>
2025-12-05 14:45:14 +0000

    feat: add storage pool test

---
+++ tests/storage_pool/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    juju = {
+      source = "registry.terraform.io/juju/juju"
+      version = "1.0.0"
+    }
+  }
+}
+
+provider "juju" {
+}
+
+module "model" {
+  topic = "storage-pool"
+  source = "../../modules/model_random"
+}
+
+output "model_name" {
+  value = module.model.name
+}
+
+resource "juju_model" "this" {
+  name = module.model.name
+}
+
+resource "juju_storage_pool" "this" {
+  name             = "pool"
+  model_uuid       = juju_model.this.uuid
+  storage_provider = "kubernetes"
+}
+
+resource "juju_application" "this" {
+  model_uuid = juju_model.this.uuid
+  name       = "db"
+  charm {
+    name    = "postgresql-k8s"
+    channel = "14/stable"
+    base    = "ubuntu@22.04"
+  }
+
+  storage_directives = {
+    "pgdata" = "1M,${juju_storage_pool.this.name}"
+  }
+}
---
+++ tests/storage_pool/run_test.go
@@ -0,0 +1,42 @@
+package qa
+
+import (
+	"os/exec"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	utils "github.com/juju/terraform-provider-juju-qa"
+)
+
+func TestQA_StoragePool(t *testing.T) {
+	// arrange
+	tfOpts := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: ".",
+		EnvVars:      utils.GetControllerEnv(t, utils.DefaultControllerName),
+		Reconfigure:  true,
+	})
+
+	// act
+	defer terraform.Destroy(t, tfOpts)
+	terraform.InitAndApply(t, tfOpts)
+
+	// assert
+	modelName := terraform.Output(t, tfOpts, "model_name")
+	cmd := exec.Command(
+		"juju", "switch",
+		modelName,
+	)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("failed juju switch: %s", out)
+	}
+
+	cmd = exec.Command(
+		"juju", "wait-for",
+		"application", "db",
+	)
+	out, err = cmd.CombinedOutput()
+	if err != nil {
+		t.Fatalf("failed juju wait-for: %s", out)
+	}
+}
