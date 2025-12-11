02cc2d1b0f1a02e45b1b5e4d91e76c5a4a9b175b
Luci Brănescu <luci.branescu@canonical.com>
2025-12-11 16:14:33 +0000

    feat: add constraint vars for storage pool plan

--- tests/storage_pool/main.tf
+++ tests/storage_pool/main.tf
@@ -41,4 +41,6 @@ resource "juju_application" "this" {
   storage_directives = {
     "pgdata" = "1M,${juju_storage_pool.this.name}"
   }
+
+  constraints = "tags=${var.shared_tags},${var.physical_tags}"
 }
